95698f463c345c41e8e5357351541db8963da3da
tuhin <tuhin153695@gmail.com>
2023-10-29 12:37:15 +0600

    footer added

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -5,6 +5,7 @@ import Container from "../Shared/Container";
 import Card from "./Card";
 import Loader from "../Shared/Loader";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Heading/Heading";
 
 const Rooms = () => {
   const [params, setParams] = useSearchParams();
@@ -13,6 +14,7 @@ const Rooms = () => {
 
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(true);
     fetch("./rooms.json")
@@ -21,10 +23,10 @@ const Rooms = () => {
         if (category) {
           const filtered = data.filter((room) => room.category === category);
           setRooms(filtered);
-        }else{
+        } else {
           setRooms(data);
         }
-       
+
         setLoading(false);
       })
       .catch((err) => console.log(err));
@@ -36,11 +38,17 @@ const Rooms = () => {
 
   return (
     <Container>
-      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {rooms.map((room, index) => (
-          <Card key={index} room={room}></Card>
-        ))}
-      </div>
+      {rooms && rooms.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms.map((room, index) => (
+            <Card key={index} room={room}></Card>
+          ))}
+        </div>
+      ) : (
+        <div className="pt-12">
+          
+        </div>
+      )}
     </Container>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
  
 import Categories from '../../components/Categories/Categories';
 import Rooms from '../../components/Rooms/Rooms';
+ 
 
 const Home = () => {
     return (
         <>
            <Categories></Categories>
-           <Rooms></Rooms> 
+           <Rooms></Rooms>
+           
         </>
     );
 };
