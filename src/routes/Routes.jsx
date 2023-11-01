cb029240ac220487d69958113954589077bd7f88
tuhin <tuhin153695@gmail.com>
2023-11-01 09:29:54 +0600

    dashboard sidebar added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,30 +4,42 @@ import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import RoomDetails from "../pages/RoomDetails/RoomDetails";
- import PrivateRoute from "./PrivateRoute"
- 
+import PrivateRoute from "./PrivateRoute";
+import DashboardLayout from "../layouts/DashboardLayout";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/room/:id',
-                element: <PrivateRoute><RoomDetails></RoomDetails></PrivateRoute>
-            }
-        ]
-    },
-    {
-        path: '/login',
-        element: <Login></Login>
-    },
-    {
-        path: '/signup',
-        element: <SignUp></SignUp>
-    }
-])
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/room/:id",
+        element: (
+          <PrivateRoute>
+            <RoomDetails></RoomDetails>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/signup",
+    element: <SignUp></SignUp>,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout></DashboardLayout>,
+    children: [
+        { path: "/dashboard/add-room", 
+        element: <p>add room</p> }
+    ],
+  },
+]);
