d4c462919aa4901a1cc1f554d49622c4d844c086
tuhin <tuhin153695@gmail.com>
2024-02-19 17:13:39 +0600

    booking modal added

--- src/components/Rooms/RoomReservation.jsx
+++ src/components/Rooms/RoomReservation.jsx
@@ -1,7 +1,14 @@
 import Button from "../Button/Button";
 import Calender from "../Rooms/Calender";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/Authprovider";
+import BookingModal from "../Modal/BookingModal";
+import { useState } from "react";
 
-const RoomReservation = () => {
+const RoomReservation = ({ roomData }) => {
+  const { user, role } = useContext(AuthContext);
+  const {isOpen, setIsOpen} = useState(false);
+  
   return (
     <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
       <div className="flex flex-row gap-1 p-4">
@@ -14,12 +21,17 @@ const RoomReservation = () => {
       </div>
       <hr />
       <div className="p-4">
-        <Button label="Reserve"></Button>
+        <Button
+          disabled={roomData.host.email === user.email}
+          label="Reserve"
+        ></Button>
       </div>
       <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
         <div>Total</div>
         <div>$ 300</div>
       </div>
+
+      <BookingModal/>
     </div>
   );
 };
